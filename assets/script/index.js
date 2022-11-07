c36226df2c9f377c440abb5d00c4424b06942d07
Guizemoreira <guisouza.moreira@gmail.com>
2022-11-07 09:27:14 +0000

    Add script bolck for IA random option

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,6 +4,20 @@ function play(){
     document.getElementById("scissors").checked == false &&
     document.getElementById("paper").checked == false){
         alert("Select an option!");
+    } else {
+        //Main block
+        var pickacard = Math.floor(Math.random()*3);
+        switch(pickacard){
+            case 0:
+                document.getElementById("iaplayer").innerHTML="IA selected Rock!";
+                break;
+            case 1:
+                document.getElementById("iaplayer").innerHTML="IA selected Scissorss!";
+                break;
+            case 2:
+                document.getElementById("iaplayer").innerHTML="IA selected Paper!";
+                break;
+        }
     }
 }
 
