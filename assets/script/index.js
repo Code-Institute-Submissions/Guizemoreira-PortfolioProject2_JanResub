716839ec87857af4c5827bf69001c5e88bd6ef7d
Guizemoreira <guisouza.moreira@gmail.com>
2022-11-07 09:28:24 +0000

    Add script bolck to test play and log the result

--- assets/script/index.js
+++ assets/script/index.js
@@ -18,6 +18,21 @@ function play(){
                 document.getElementById("iaplayer").innerHTML="IA selected Paper!";
                 break;
         }
+        //Checking the winner
+        if ((document.getElementById("rock").checked == true && pickacard == 0) || 
+        (document.getElementById("scissors").checked == true && pickacard == 1) || 
+        (document.getElementById("paper").checked == true && pickacard == 2)){
+            document.getElementById("iawin").innerHTML="";
+            document.getElementById("score").innerHTML="Draw!";
+        } else if ((document.getElementById("rock").checked == true && pickacard == 1) || 
+        (document.getElementById("scissors").checked == true && pickacard == 2) || 
+        (document.getElementById("paper").checked == true && pickacard == 0)){
+            document.getElementById("iawin").innerHTML="";
+            document.getElementById("score").innerHTML="You Win!";
+        } else {
+            document.getElementById("score").innerHTML="";
+            document.getElementById("iawin").innerHTML="You Lose!";
+        }
     }
 }
 
